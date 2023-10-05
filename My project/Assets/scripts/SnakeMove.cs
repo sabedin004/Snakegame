acdf5cd8d8710e198ed75621eeff1a2d59e5c5f2
sabedin004 <144362658+sabedin004@users.noreply.github.com>
2023-10-05 11:47:09 -0400

    Key Movements Updated
    Key Movements Updated to be more like the actual snake game

--- My project/Assets/scripts/SnakeMove.cs
+++ My project/Assets/scripts/SnakeMove.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class SnakeMove : MonoBehaviour
 {
     //variables
     private Vector2 direction; //control direction of movement
+    public bool goingUp;
+    public bool goingDown;
+    public bool goingLeft;
+    public bool goingRight; 
+
+
 
     List<Transform> segments;   // stores all parts of the body of the snake
     public Transform bodyPrefab; // variable to store the body
@@ -21,28 +28,46 @@ public class SnakeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))    //when W key is pressed
+        if (Input.GetKeyDown(KeyCode.W) && goingDown != true)    //when W key is pressed
         {
             direction = Vector2.up;        //go up
+            goingUp = true;
+            goingDown = false;
+            goingLeft = false;
+            goingRight = false;
         }
 
 
         else if (Input.GetKeyDown(KeyCode.A))    //when A key is pressed
         {
             direction = Vector2.left;        //go left
+            goingUp = false;
+            goingDown = false;
+            goingLeft = true;   
+            goingRight = false;
         }
 
 
-        else if (Input.GetKeyDown(KeyCode.S))    //when S key is pressed
+        else if (Input.GetKeyDown(KeyCode.S) && goingUp != true )    //when S key is pressed
         {
             direction = Vector2.down;        //go down
+            goingUp = false;
+            goingDown = true;
+            goingLeft = false;
+            goingRight = false;
         }
-
-
         else if (Input.GetKeyDown(KeyCode.D))    //when D key is pressed
         {
             direction = Vector2.right;        //go right
+            goingUp = false;
+            goingDown = false;
+            goingLeft = false;
+            goingRight = true;
+
         }
+
+
+       
     }
     //FixedUpdate is called at a fix interval
     void FixedUpdate()
@@ -75,13 +100,15 @@ public class SnakeMove : MonoBehaviour
     //Funtion for collision
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Food")      //the red
+        if (other.tag == "Food")      // check if the other object is the food
         {
-            Grow();
+            Grow();             //turn the grow function
         }
-        else if (other.tag == "Obstacle")
+        else if (other.tag == "Obstacle")      //check if the other object is an obstacle
         {
+            //Debug.Log("Hit")
             Debug.Log("Hit");
+            SceneManager.LoadScene("Endscene");                //change to end scene
         }
 
     }
